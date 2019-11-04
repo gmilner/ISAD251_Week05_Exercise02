dc8516891d1c02ec3d30548c2ed286f2217b82b3
George Milner <gmilner@SOCNET.SOC.PLYMOUTH.AC.UK>
2019-11-04 14:32:48 +0000

    Created solution

---
+++ ISAD251_Week05/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ISAD251_Week05.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
